4ace6e55c42ec3b6515edc061b565afa20a59384
Manish4586 <Manish.n.manish45@gmail.com>
2018-02-04 00:56:12 +0530

    Auto patch MTK : M bring up

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,52 +1,43 @@
-#!/bin/bash
-
+ #!/bin/bash
+ 
  cd ../../../..
  cd packages/apps/Settings
  patch -p1 -b < ../../../device/Lava/IrisX8/patches/dev_info.patch
  git clean -f -d
- cd ../../..
+ cd ../..
  cd system/core
  patch -p1 < ../../device/Lava/IrisX8/patches/system_core.patch
- cd ..
- cd bt
+ cd ../..
+ cd system/bt
  patch -p1 < ../../device/Lava/IrisX8/patches/system_bt.patch
  cd ..
  cd netd
  patch -p1 < ../../device/Lava/IrisX8/patches/system_netd.patch
- cd ..
- cd sepolicy
- patch -p1 < ../../device/Lava/IrisX8/patches/system_sepolicy.patch
  cd ..
  cd vold
  patch -p1 < ../../device/Lava/IrisX8/patches/system_vold.patch
  cd ../..
  cd frameworks/av
  patch -p1 < ../../device/Lava/IrisX8/patches/frameworks_av.patch
- patch -p1 < ../../device/Lava/IrisX8/patches/frameworks_av_2.patch
  cd ..
  cd base
  patch -p1 < ../../device/Lava/IrisX8/patches/frameworks_base.patch
  cd ..
+ cd rs
+ patch -p1 < ../../device/Lava/IrisX8/patches/frameworks_rs.patch
+ cd ..
  cd native
  patch -p1 < ../../device/Lava/IrisX8/patches/frameworks_native.patch
  cd ..
- cd opt/telephony
+ cd opt/telephony/
  patch -p1 < ../../../device/Lava/IrisX8/patches/frameworks_opt_telephony.patch
  cd ../../..
- cd external/icu
- patch -p1 < ../../device/Lava/IrisX8/patches/external_icu.patch
+ cd external/sepolicy
+ patch -p1 < ../../device/Lava/IrisX8/patches/external_sepolicy.patch
  cd ..
  cd wpa_supplicant_8
- patch -p1 < ../../device/Lava/IrisX8/patches/wpa_supplicant_8.patch
+ patch -p1 < ../../device/Lava/IrisX8/patches/external_wpa_supplicant_8.patch
  cd ../..
- cd packages/apps/FMRadio
- patch -p1 < ../../../device/Lava/IrisX8/patches/packages_apps_FMRadio.patch
- cd ../../..
-# cd vendor/codeaurora/telephony
-# patch -p1 -p1 < ../../../device/Lava/IrisX8/patches/vendor_codeaurora_telephony.patch
-# cd ../../..
- cd packages/services/Telephony
- patch -p1 -p1 < ../../../device/Lava/IrisX8/patches/packages_services_Telephony.patch
- echo Cleaning Directory For Building
- cd ../../..
- echo Successfuly patched! Go ahead and start building! @ Manish_4586
+ echo Cleaning For Building
+ echo PATCHED (^_^) brunch IrisX8 
+ by Manish_4586!
